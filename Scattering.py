7478fad612a1d91668e28ff05e2435dd85c6d4f9
Bucket1572 <36808515+Bucket1572@users.noreply.github.com>
2019-10-19 21:50:18 +0900

    Add files via upload

---
+++ Scattering.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from netCDF4 import Dataset
+dataset = Dataset('world_oscar_vel_5d2019.nc.gz.nc4')
+
+"""Read the `uf` data to numpy array."""
+
+uf = -dataset.variables['uf'][0][0].data
+vf = dataset.variables['vf'][0][0].data
+
+lon = dataset.variables['longitude']
+lat = dataset.variables['latitude']
+
+import matplotlib.pyplot as plt
+import numpy as np
+import random as rd
+
+x_axis = np.arange(0, 360, 1)
+y_axis = np.arange(139, -1, 1)
+
+factor = 1 # 상수
+
+def findNext(latitude, longitude): # 위도, 경도
+    global factor, uf, vf, lon, lat
+    lat2 = 140 - (grid(latitude, lat[0]) % 140)
+    lon2 = grid(longitude, lon[0]) % 360
+    u2 = uf[lat2][lon2]
+    v2 = vf[lat2][lon2]
+    if np.isnan(u2) or np.isnan(v2):
+        return False
+    finPhi = latitude + factor * v2
+    finTheta = longitude + factor * u2
+    return (finPhi, finTheta) # 위도, 경도
+
+def grid(A, org):
+    return int(round(A - 0.5) + 0.5 - org) 
+    
+def findStart():
+    global uf, vf
+    Theta = rd.uniform(20.5, 379.5)
+    Phi = rd.uniform(-69.5, 69.5)
+    while np.isnan(uf[Theta][Phi]):
+        Theta = rd.uniform(20.5, 379.5)
+        Phi = rd.uniform(-69.5, 69.5)
+    return Phi, Theta # 위도, 경도
+
+def euclidD(A, B):
+    return pow((A[0] - B[0])**2 + (A[1] - B[1])**2, 0.5)
+
+plt.figure(figsize=(12.8, 9.6))
+plt.quiver(lon, lat, uf, vf)
+prevposition = (24, 158) # lat & lon
+routeX = [prevposition[1]] # lon
+routeY = [prevposition[0]] # lat
+print(f"Start Point : {prevposition}")
+for i in range(1000000) :
+    position = findNext(prevposition[0], prevposition[1])
+    if not position or euclidD(position, prevposition) <= 1e-5:
+        break
+    routeY.append(position[0]) # lat
+    routeX.append(position[1]) # 
+    prevposition = position
+plt.scatter(routeX, routeY, 0.1)
+plt.show()
+print("E")
+
